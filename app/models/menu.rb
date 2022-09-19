083ffa10b5efe6c8e04d7967d0afed843865c209
Cris <62491150+Entenduu@users.noreply.github.com>
2022-09-19 19:36:54 -0400

    created restaurant model, created join table, set associations and validations, created and ran test (#2)
    Created restaurant model
    created join table
    set associations and validations
    created and ran tests

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,6 @@
 class Menu < ApplicationRecord
-    has_many :menu_items
+    has_many :menu_items_menus
+    has_many :menu_items, through: :menu_items_menus
+    belongs_to :restaurant
     validates :name, presence: true
-
 end
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe MenuItem, type: :model do
   describe "Associations" do
-    it { should belong_to(:menu).without_validating_presence }
+    it { should have_many(:menus)}
   end
 
   describe "Validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:price_cents) }
     it { should validate_presence_of(:menu_id) }
+    it { should validate_uniqueness_of(:name) }
   end
 end
